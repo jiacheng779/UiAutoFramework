1b984f9f1ff844ec154b5a23d474d8cf9446b808
baijiacheng1 <2633547951@qq.com>
2023-11-28 21:38:22 +0800

    全局URL、数据库配置等

---
+++ commons/settings.py
@@ -0,0 +1,39 @@
+# env config
+ENV = 'test'
+
+# test url
+WEB_TEST_BASE_URL = "https://www.test.com"
+WEB_DEV_BASE_URL = "https://www.dev.com"
+WEB_UAT_BASE_URL = "https://www.uat.com"
+
+# test sp orverwiret
+WEB_SP_BASE_URL = "https://finmpgc-preuat.ey.com.cn/login/OALogin.jsp#/?_key=ook2gf"
+
+TIMES = 10
+POLL_FREQUENCY = 0.5
+
+# webdriver
+DRIVER_NAME = "chrome"
+
+# webdriver_path
+CHROMEDRIVER = r"drivers\chromedriver.exe"
+FIREFOXDRIVER = r""
+EDGEDRIVER = r"drivers\msedgedriver.exe"
+
+# mysql
+MYSQL_HOST = ""
+MYSQL_USER = ""
+MYSQL_PASSWORD = ""
+MYSQL_PORT = ""
+MYSQL_DB_NAME = ""
+MYSQL_CHARSET = ""
+DB_INIT_PATH = ""
+
+# mongodb
+MONGODB_HOST = ""
+MONGODB_USER = ""
+MONGODB_PASSWORD = ""
+MONGODB_PORT = ""
+
+# redis
+REDIS_HOST = ""
